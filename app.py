147be5f8ae693725febd244b4896af1463fd7a68
Jasondmcgee <jasondmcgee17@gmail.com>
2019-09-24 14:32:53 -0700

    added mongo db to the model

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 playlists = [
     {'title': 'Cat Videos', 'description': 'Cats acting weird'},
-    {'title': '80\'s Music', 'description': 'Don\'t stop believing!'}
+    {'title': '80\'s Music', 'description': 'Don\'t stop believing!'},
+    {'title': 'changes to array', 'description': 'does it work?'}
 ]
 
 
